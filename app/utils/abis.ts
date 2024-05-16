afbb7ee3ae0d9384bc0e9d11df4e8d109f383299
daohuytuan <tuandh95@gmail.com>
2024-05-17 03:10:58 +0700

    update

---
+++ app/utils/abis.ts
@@ -0,0 +1,103 @@
+export const AccountABI = [
+  {
+    inputs: [{ internalType: "address", name: "_owner", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      { internalType: "string", name: "_token", type: "string" },
+      { internalType: "uint256", name: "_value", type: "uint256" },
+      { internalType: "uint256", name: "_price", type: "uint256" },
+    ],
+    name: "create_order",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "_id", type: "uint256" }],
+    name: "get_order_by_id",
+    outputs: [
+      {
+        components: [
+          { internalType: "uint256", name: "id", type: "uint256" },
+          { internalType: "string", name: "token", type: "string" },
+          { internalType: "uint256", name: "value", type: "uint256" },
+          { internalType: "uint256", name: "price", type: "uint256" },
+          { internalType: "bool", name: "existed", type: "bool" },
+        ],
+        internalType: "struct AccountState.Order",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "get_orders",
+    outputs: [
+      {
+        components: [
+          { internalType: "uint256", name: "id", type: "uint256" },
+          { internalType: "string", name: "token", type: "string" },
+          { internalType: "uint256", name: "value", type: "uint256" },
+          { internalType: "uint256", name: "price", type: "uint256" },
+          { internalType: "bool", name: "existed", type: "bool" },
+        ],
+        internalType: "struct AccountState.Order[]",
+        name: "",
+        type: "tuple[]",
+      },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "_id", type: "uint256" },
+      { internalType: "string", name: "_token", type: "string" },
+      { internalType: "uint256", name: "_value", type: "uint256" },
+      { internalType: "uint256", name: "_price", type: "uint256" },
+    ],
+    name: "update_order",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+export const FactoryABI = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    inputs: [],
+    name: "create_account",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "get_account",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
